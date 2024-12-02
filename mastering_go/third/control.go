677bf3b83b6bee9cb4611505de64801cce7cbdc6
SusanHex <suehex4027@gmail.com>
2024-12-02 16:17:00 +0000

    Add control.go

---
+++ mastering_go/third/control.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Please provide an argument :(")
+		return
+	}
+	argument := os.Args[1]
+	switch argument {
+	case "0":
+		fmt.Println("Zero! :)")
+	case "1":
+		fmt.Println("One!!!!!!!")
+	case "2", "3", "4":
+		fmt.Println("You entered 3, 4 or 5")
+	default:
+		fmt.Println("Value: ", argument)
+	}
+}
