34ad759f11a2682d04daed0d9de80ade5b0316fe
SusanHex <suehex4027@gmail.com>
2024-12-02 18:58:21 +0000

    Update control.go with different functionality

--- mastering_go/third/control.go
+++ mastering_go/third/control.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -11,14 +12,19 @@ func main() {
 		return
 	}
 	argument := os.Args[1]
-	switch argument {
-	case "0":
+	value, err := strconv.Atoi(argument)
+	if err != nil {
+		fmt.Println("Can not convert argument to int: ", argument)
+		return
+	}
+	switch {
+	case value == 0:
 		fmt.Println("Zero! :)")
-	case "1":
-		fmt.Println("One!!!!!!!")
-	case "2", "3", "4":
-		fmt.Println("You entered 2, 3 or 4")
+	case value > 0:
+		fmt.Println("Positive Integer")
+	case value < 0:
+		fmt.Println("Negative Integer")
 	default:
-		fmt.Println("Value: ", argument)
+		fmt.Println("Something went horribly wrong. Here is the argument: ", argument)
 	}
 }
