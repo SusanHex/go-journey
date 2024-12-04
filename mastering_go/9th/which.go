2aaed489c51e1a9fc74d77dd23d0f1d202909076
SusanHex <suehex4027@gmail.com>
2024-12-04 23:00:00 +0000

    Finish which.go

---
+++ mastering_go/9th/which.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide an argument")
+		return
+	}
+	file := arguments[1]
+	path := os.Getenv("PATH")
+	pathSplit := filepath.SplitList(path)
+	for _, directory := range pathSplit {
+		fullPath := filepath.Join(directory, file)
+		fileInfo, err := os.Stat(fullPath)
+		if err != nil {
+			continue
+		}
+		mode := fileInfo.Mode()
+		// Is this a regular file?
+		if !mode.IsRegular() {
+			continue
+		}
+		//Is this an executable?
+		if mode&0111 != 0 {
+			fmt.Println(fullPath)
+			return
+		}
+	}
+
+}
