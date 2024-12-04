e6cb3b0d9d041f1539bb05da67f690ebb4d39969
SusanHex <suehex4027@gmail.com>
2024-12-04 21:54:21 +0000

    Edit file

--- mastering_go/6th/cla.go
+++ mastering_go/6th/cla.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"fmt"
 	"os"
@@ -11,4 +12,26 @@ func main() {
 		fmt.Println("Dang you! I need an argument! Nooooo!!!!")
 		return
 	}
+	var min, max float64
+	var initalized = 0
+	for i := 1; i < len(arguments); i++ {
+		n, err := strconv.ParseFloat(arguments[i], 64)
+		if err != nil {
+			fmt.Printf("Failed to convert \"%s\" to float. Error: \"%s\"\n", arguments[i], err)
+			continue
+		}
+		if initalized == 0 {
+			min = n
+			max = n
+			initalized = 1
+			continue
+		}
+		if n < min {
+			min = n
+		} else if n > max {
+			max = n
+		}
+	}
+	fmt.Println("Min:", min)
+	fmt.Println("Max:", max)
 }
