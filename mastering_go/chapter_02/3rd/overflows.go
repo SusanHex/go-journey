27ea8d9e570ccdf125aa0f393656a8ff17b13344
SusanHex <suehex4027@gmail.com>
2024-12-10 15:38:37 +0000

    Add overflow.go

---
+++ mastering_go/chapter_02/3rd/overflows.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Max Int:", math.MaxInt)
+	fmt.Println("Min Int:", math.MinInt)
+	// 	i := 0
+	// 	for {
+	// 		if i == math.MaxInt {
+	// 			fmt.Println("Largest possible int before overflow", i)
+	// 			break
+	// 		}
+	// 		i = i + 1
+	// 		fmt.Println(i)
+	// 	}
+	// 	for {
+	// 		if i == math.MinInt {
+	// 			fmt.Println("Smallest possible int before underflow:", i)
+	// 			break
+	// 		}
+	// 	}
+	// 	i = i - 1
+	// fmt.Println(i)
+}
