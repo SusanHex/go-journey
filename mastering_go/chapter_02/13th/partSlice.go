5e7f11a97f137f8afbd4ad90f5f776c9c0bb4309
SusanHex <suehex4027@gmail.com>
2024-12-11 21:15:29 +0000

    Add partSlice.go

---
+++ mastering_go/chapter_02/13th/partSlice.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	aSlice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(aSlice)
+	l := len(aSlice)
+	fmt.Println(aSlice[:5])
+	fmt.Println(aSlice[0:5])
+	fmt.Println(aSlice[l-2 : l])
+	fmt.Println(aSlice[l-2:])
+	t := aSlice[0:5:10]
+	fmt.Println(len(t), cap(t))
+	t = aSlice[2:5:10]
+	fmt.Println(len(t), cap(t))
+	t = aSlice[:5:6]
+	fmt.Println(len(t), cap(t))
+}
