23cdd8f27c00b60ee3bd3525089ee44ca1fe887d
SusanHex <suehex4027@gmail.com>
2024-12-11 16:33:38 +0000

    Add complete convertTimes.go

--- mastering_go/chapter_02/8th/convertTimes.go
+++ mastering_go/chapter_02/8th/convertTimes.go
@@ -11,11 +11,14 @@ func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("Please provide a time")
 	}
-	input_time, err := time.Parse("RFC822", os.Args[1])
+	input_time, err := time.Parse("02 January 2006 15:04", os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(input_time)
-	loc, _ := time.LoadLocation("America/Denver")
-	fmt.Printf("Denver Time: %s\n", loc)
+	MST, _ := time.LoadLocation("America/Denver")
+	EST, _ := time.LoadLocation("America/New_York")
+	fmt.Printf("MST: %v\n", input_time.In(MST))
+	fmt.Printf("EST: %v\v", input_time.In(EST))
+
 }
