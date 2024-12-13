ef47587ab4af5db59c485e8e3b87ea9a653eedbd
SusanHex <suehex4027@gmail.com>
2024-12-13 18:17:37 +0000

    Finish pointers.go

--- mastering_go/chapter_02/19th/pointers.go
+++ mastering_go/chapter_02/19th/pointers.go
@@ -24,5 +24,31 @@ func bothPointers(x *float64) *float64 {
 func main() {
 	var f float64 = 12.123
 	fmt.Println("Memory address of f:", &f)
+	//pointer to f
+	fP := &f
+	fmt.Println("Memory address of f, (accessed through fP pointer):", fP)
+	fmt.Println("Value of f:", *fP)
+	processPointer(fP)
+	fmt.Printf("Value of f: %.2f\n", f)
+	//value of f does not change here
+	x := returnPointer(f)
+	//value of f does not change here
+	fmt.Printf("value of x: %.2f\n", *x)
+	xx := bothPointers(fP)
+	fmt.Printf("Value of xx: %.2f\n", *xx)
+	//check for empty structure
+	var k *aStructure
+	//this is nil because currently k points to nowhere
+	fmt.Println(k)
+	//therefore you are allowed to do this:
+	if k == nil {
+		k = new(aStructure)
+	}
+	fmt.Printf("%+v\n", k)
+	if k != nil {
+		fmt.Println("k is not nil!")
+		fmt.Println(k.field1)
+		fmt.Println(k.field2)
+	}
 
 }
