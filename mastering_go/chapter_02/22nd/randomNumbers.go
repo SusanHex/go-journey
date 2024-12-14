47291c95435ed4a549813a96418b0c5004134d02
SusanHex <suehex4027@gmail.com>
2024-12-14 06:39:10 +0000

    Make seed argument optional in randomNumbers.go

--- mastering_go/chapter_02/22nd/randomNumbers.go
+++ mastering_go/chapter_02/22nd/randomNumbers.go
@@ -15,15 +15,24 @@ func main() {
 	min := 0
 	max := 100
 	count := 100
+	seed := 0
 	if len(os.Args) == 1 {
 		fmt.Println("No arguments provided, using defaults")
 	} else {
-		if len(os.Args) < 5 {
-			fmt.Println("Please provide at least four arguments. Please provide <min> <max> <count> <seed>")
+		if len(os.Args) < 4 {
+			fmt.Println("Please provide at least three arguments. Please provide <min> <max> <count> [<seed>]")
 			os.Exit(2)
 		} else if len(os.Args) > 5 {
-			fmt.Println("Too many Arguments provided. please provide <min> <max> <count> <seed>")
+			fmt.Println("Too many Arguments provided. please provide <min> <max> <count> [<seed>]")
 			os.Exit(1)
+		} else if len(os.Args) == 5 {
+			temp_seed, error := strconv.Atoi(os.Args[4])
+			if error != nil {
+				fmt.Println(error)
+				os.Exit(5)
+			}
+			seed = temp_seed
+			rand.Seed(int64(seed))
 		}
 		temp_min, error := strconv.Atoi(os.Args[1])
 		if error != nil {
@@ -40,17 +49,12 @@ func main() {
 			fmt.Println(error)
 			os.Exit(5)
 		}
-		seed, error := strconv.Atoi(os.Args[4])
-		if error != nil {
-			fmt.Println(error)
-			os.Exit(5)
-		}
-		rand.Seed(int64(seed))
+
 		min = temp_min
 		max = temp_max
 		count = temp_count
 	}
-	for i := 0; i <= count; i++ {
+	for i := 0; i < count; i++ {
 		fmt.Printf("%d ", random(min, max))
 	}
 	fmt.Println()
