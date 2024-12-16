2298b694482f4ee4ed45479ae106ba53067af0c5
SusanHex <suehex4027@gmail.com>
2024-12-16 17:49:11 +0000

    Integrate normalize function and add randomFlat function to stats.go

--- mastering_go/chapter_01/15th/stats.go
+++ mastering_go/chapter_01/15th/stats.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
 	"strconv"
 )
@@ -18,16 +19,41 @@ func normalize(data []float64, mean float64, stdDev float64) []float64 {
 	return normalized
 }
 
+func randomFloat(min, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
+
 func main() {
 	arguments := os.Args
-	if len(arguments) == 1 {
-		fmt.Println("Need More Arguments :(")
-		return
-	}
+	data := make([]float64, len(arguments)-1)
 	var min, max float64
 	var initialized = 0
 	nValues := 0
 	var sum float64
+
+	if len(arguments) == 1 {
+		fmt.Println("No arguments provided, generating default")
+		data = make([]float64, 10)
+		nValues = 10
+		for i := range data {
+			data[i] = randomFloat(1, 100)
+			sum += data[i]
+			if initialized == 0 {
+				min = data[i]
+				max = data[i]
+				initialized = 1
+				continue
+			}
+			if data[i] < min {
+				min = data[i]
+			}
+			if data[i] > max {
+				max = data[i]
+			}
+		}
+
+	}
+
 	for i := 1; i < len(arguments); i++ {
 		n, err := strconv.ParseFloat(arguments[i], 64)
 		if err != nil {
@@ -47,6 +73,7 @@ func main() {
 		if n > max {
 			max = n
 		}
+		data[i-1] = n
 	}
 	fmt.Println("Number of values:", nValues)
 	fmt.Println("Min:", min)
@@ -58,14 +85,13 @@ func main() {
 	fmt.Printf("Mean Value: %.5f\n", meanValue)
 	// standard deviation
 	var squared float64
-	for i := 1; i < len(arguments); i++ {
-		n, err := strconv.ParseFloat(arguments[i], 32)
-		if err != nil {
-			continue
-		}
+	for i := 1; i < len(data); i++ {
+		n := data[i]
 		nValues = nValues + 1
 		squared = squared + math.Pow((n-meanValue), 2)
 	}
 	standardDeviation := math.Sqrt(squared / float64(nValues))
+	normalizedData := normalize(data, meanValue, standardDeviation)
 	fmt.Printf("Standard Deviation: %.5f\n", standardDeviation)
+	fmt.Printf("Normalized: %v\n", normalizedData)
 }
