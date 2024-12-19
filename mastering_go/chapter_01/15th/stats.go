ccb5251c039c9f06c8955793a54ea33cbab2fe5c
SusanHex <suehex4027@gmail.com>
2024-12-19 17:35:05 +0000

    Upgrade stats.go with chapter 3 modifacations

--- mastering_go/chapter_01/15th/stats.go
+++ mastering_go/chapter_01/15th/stats.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
 	"math"
 	"math/rand"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -23,75 +26,61 @@ func randomFloat(min, max float64) float64 {
 	return min + rand.Float64()*(max-min)
 }
 
-func main() {
-	arguments := os.Args
-	data := make([]float64, len(arguments)-1)
-	var min, max float64
-	var initialized = 0
-	nValues := 0
-	var sum float64
-
-	if len(arguments) == 1 {
-		fmt.Println("No arguments provided, generating default")
-		data = make([]float64, 10)
-		nValues = 10
-		for i := range data {
-			data[i] = randomFloat(1, 100)
-			sum += data[i]
-			if initialized == 0 {
-				min = data[i]
-				max = data[i]
-				initialized = 1
-				continue
-			}
-			if data[i] < min {
-				min = data[i]
-			}
-			if data[i] > max {
-				max = data[i]
-			}
-		}
-
+func stdDev(x []float64) (float64, float64) {
+	sum := 0.0
+	for _, val := range x {
+		sum = sum + val
 	}
+	meanValue := sum / float64(len(x))
+	// fmt.Printf("Mean value: %.5f\n", meanValue)
+	var squarded float64
+	for i := 0; i < len(x); i++ {
+		squarded = squarded + math.Pow((x[i]-meanValue), 2)
+	}
+	standardDeviation := math.Sqrt(squarded / float64(len(x)))
+	return meanValue, standardDeviation
+}
 
-	for i := 1; i < len(arguments); i++ {
-		n, err := strconv.ParseFloat(arguments[i], 64)
+func readFile(filepath string) ([]float64, error) {
+	_, err := os.Stat(filepath)
+	if err != nil {
+		return nil, err
+	}
+	f, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	values := make([]float64, 0)
+	for _, line := range lines {
+		temp, err := strconv.ParseFloat(line[0], 64)
 		if err != nil {
-			continue
+			log.Println("Error reading:", line[0], err)
 		}
-		nValues = nValues + 1
-		sum = sum + n
-		if initialized == 0 {
-			min = n
-			max = n
-			initialized = 1
-			continue
-		}
-		if n < min {
-			min = n
-		}
-		if n > max {
-			max = n
-		}
-		data[i-1] = n
+		values = append(values, temp)
 	}
-	fmt.Println("Number of values:", nValues)
-	fmt.Println("Min:", min)
-	fmt.Println("Max:", max)
-	if nValues == 0 {
-		return
+	return values, nil
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		log.Println("Need one argument!")
 	}
-	meanValue := sum / float64(nValues)
-	fmt.Printf("Mean Value: %.5f\n", meanValue)
-	// standard deviation
-	var squared float64
-	for i := 1; i < len(data); i++ {
-		n := data[i]
-		nValues = nValues + 1
-		squared = squared + math.Pow((n-meanValue), 2)
+	file := os.Args[1]
+	values, err := readFile(file)
+	if err != nil {
+		log.Println("Error reading:", file, err)
 	}
-	standardDeviation := math.Sqrt(squared / float64(nValues))
-	normalizedData := normalize(data, meanValue, standardDeviation)
+	sort.Float64s(values)
+	fmt.Println("Number of values:", len(values))
+	fmt.Println("Min:", values[0])
+	fmt.Println("Max:", values[len(values)-1])
+	meanValue, standardDeviation := stdDev(values)
 	fmt.Printf("Standard Deviation: %.5f\n", standardDeviation)
-	fmt.Printf("Normalized: %v\n", normalizedData)
+	normalized := normalize(values, meanValue, standardDeviation)
+	fmt.Println("Normalized", normalized)
 }
