491479347297a9acdddd7ed54856b964f82db6b1
SusanHex <suehex4027@gmail.com>
2024-12-22 05:23:18 +0000

    Change on cmp.Compare to cmd.Less in cmpPackage.go

--- mastering_go/chapter_04/cmpPackage/cmpPackage.go
+++ mastering_go/chapter_04/cmpPackage/cmpPackage.go
@@ -9,5 +9,5 @@ func main() {
 	fmt.Println(cmp.Compare(5, 4))
 	fmt.Println(cmp.Compare(4, 5))
 	fmt.Println(cmp.Compare(4, 5.1))
-	fmt.Println(cmp.Compare('a', 'b'))
+	fmt.Println(cmp.Less('a', 'b'))
 }
