06b610ae92f31506568a1c24bca37a1a93f05051
SusanHex <suehex4027@gmail.com>
2024-12-22 17:49:33 +0000

    Complete excercise for chapter 4. Make the double linked list and add search, print, and delete functions

--- mastering_go/chapter_04/structures/structures.go
+++ mastering_go/chapter_04/structures/structures.go
@@ -2,13 +2,63 @@ package main
 
 import "fmt"
 
-type node[T any] struct {
-	data T
-	next *node[T]
+type node[T comparable] struct {
+	data     T
+	next     *node[T]
+	previous *node[T]
 }
 
-type list[T any] struct {
+type list[T comparable] struct {
 	start *node[T]
+}
+
+func (l *list[T]) delete(inputNode *node[T]) {
+
+	current_node := l.start
+	for {
+		if current_node == nil {
+			return
+		}
+		if current_node == inputNode {
+			break
+		}
+		current_node = current_node.next
+	}
+	previous_node := current_node.previous
+	next_node := current_node.next
+	if previous_node != nil {
+		previous_node.next = next_node
+	}
+	if next_node != nil {
+		next_node.previous = next_node
+	}
+	current_node.next = nil
+	current_node.previous = nil
+}
+
+func (l *list[T]) print() {
+	current_node := l.start
+	for {
+		if current_node == nil {
+			break
+		}
+		fmt.Print(current_node.data, " ")
+		current_node = current_node.next
+	}
+	fmt.Println()
+}
+
+func (l *list[T]) search(term T) *node[T] {
+	current_node := l.start
+	for {
+		if current_node == nil {
+			return nil
+		}
+		if current_node.data == term {
+			return current_node
+		}
+		current_node = current_node.next
+	}
 }
 
 func (l *list[T]) add(data T) {
@@ -18,11 +68,13 @@ func (l *list[T]) add(data T) {
 	}
 	if l.start == nil {
 		l.start = &n
+		l.start.previous = nil
 		return
 	}
 
 	if l.start.next == nil {
 		l.start.next = &n
+		l.start.next.previous = l.start
 		return
 	}
 	temp := l.start
@@ -46,8 +98,16 @@ func main() {
 		if curr == nil {
 			break
 		}
-		fmt.Println("Index:", index, "Current node:", &curr, "Data:", curr.data, "Next node:", curr.next)
+		fmt.Println("Index:", index, "Current node:", curr, "Data:", curr.data, "Next node:", curr.next)
 		curr = curr.next
 		index++
+	}
+	myList.print()
+	tempNode := myList.search(2)
+	if tempNode != nil {
+		fmt.Println("Search list for 2, found node at:", tempNode)
+		myList.delete(tempNode)
+		fmt.Println("Deleted found node")
+		myList.print()
 	}
 }
