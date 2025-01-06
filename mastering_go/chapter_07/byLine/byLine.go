4a02423ea406cdf980030d782cd8f80822592505
SusanHex <suehex4027@gmail.com>
2025-01-06 02:10:55 +0000

    Add byLine.go

---
+++ mastering_go/chapter_07/byLine/byLine.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func lineByLine(input io.Reader) error {
+	r := bufio.NewReader(input)
+	for {
+		line, err := r.ReadString('\n')
+		if err == io.EOF {
+			if len(line) != 0 {
+				fmt.Println(line)
+			}
+			break
+		}
+		if err != nil {
+			fmt.Printf("error reading file: %s\n", err)
+			return err
+		}
+		fmt.Print(line)
+	}
+	return nil
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Please provide a file as input")
+		os.Exit(1)
+	}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	lineByLine(file)
+}
