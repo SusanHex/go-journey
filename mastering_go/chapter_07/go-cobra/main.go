cceb22f88190db457d28e37d78731008447492ec
SusanHex <suehex4027@gmail.com>
2025-01-27 20:31:33 +0000

    Add go-cobra part of chapter 7

---
+++ mastering_go/chapter_07/go-cobra/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/SusanHex/go-journey/mastering_go/chapter_07/go-cobra/cmd"
+
+func main() {
+	cmd.Execute()
+}
