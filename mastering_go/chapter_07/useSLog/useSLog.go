0a04aa90ba718f52cf53967702bcbce3ffa5c6b3
SusanHex <suehex4027@gmail.com>
2025-01-29 17:56:01 +0000

    Add useSLog.go

---
+++ mastering_go/chapter_07/useSLog/useSLog.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+func main() {
+	slog.Error("This is an ERROR message")
+	slog.Debug("This is a DEBUG message")
+	slog.Info("This is a INFO message")
+	slog.Warn("This is a WARNING message")
+	logLevel := &slog.LevelVar{}
+	fmt.Println("Log Level:", logLevel)
+	// text handler for log messages
+	opts := &slog.HandlerOptions{
+		Level: logLevel,
+	}
+	handler := slog.NewTextHandler(os.Stdout, opts)
+	logger := slog.New(handler)
+	logLevel.Set(slog.LevelDebug)
+	logger.Debug("This is DEBUG message")
+	// JSON handler
+	logJSON := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logJSON.Error("ERROR message in JSON")
+
+}
